023be61cbf6b8f6e907519889967ade3346b02bb
Denys <denis.shmatkov95@gmail.com>
2025-06-17 22:19:12 +0300

    додав всі селекти для філтрів та стилізував їх

--- client/src/components/VacancyFilter/index.jsx
+++ client/src/components/VacancyFilter/index.jsx
@@ -2,6 +2,8 @@ import React, { useContext } from "react";
 
 import { VacancyContext } from "../../context/VacancyContext";
 
+import "./style.css";
+
 const VacancyFilter = () => {
   const {
     vacancies,
@@ -17,14 +19,34 @@ const VacancyFilter = () => {
 
   return (
     <>
-      <select value={selectedCity} onChange={(e) => setSelectedCity(e.target.value)}>
-        <option value="">Оберіть місто / Всі міста</option>
-        {uniqueCities.map((item) => (
-          <option key={item} value={item}>
-            {item}
-          </option>
-        ))}
-      </select>
+      <div className="filter_field">
+        <select value={selectedCity} onChange={(e) => setSelectedCity(e.target.value)}>
+          <option value="">Оберіть місто / Всі міста</option>
+          {uniqueCities.map((item) => (
+            <option key={item} value={item}>
+              {item}
+            </option>
+          ))}
+        </select>
+        <select value={selectedCity} onChange={(e) => setSelectedCity(e.target.value)}>
+          <option value="">Оберіть місто / Всі міста</option>
+          {uniqueCities.map((item) => (
+            <option key={item} value={item}>
+              {item}
+            </option>
+          ))}
+        </select>
+        <select value={selectedCity} onChange={(e) => setSelectedCity(e.target.value)}>
+          <option value="">Оберіть місто / Всі міста</option>
+          {uniqueCities.map((item) => (
+            <option key={item} value={item}>
+              {item}
+            </option>
+          ))}
+        </select>
+
+        <button className="clear_filter">Очистити фільтри</button>
+      </div>
     </>
   );
 };
